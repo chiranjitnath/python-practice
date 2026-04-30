1205a5e311b3d081fe341cc2dbca85b2fc2f2096
chiranjitnath <chiranjitnath454@gmail.com>
2026-04-30 23:24:13 +0530

    Delete problem_level_1 directory

--- problem_level_1/Sum_to_N.py
+++
@@ -1,9 +0,0 @@
-number = int(input("enter your number : "))
-final_val = 0
-for i in range (number):
-    substraction = number - i
-    final_val += substraction
-
-print(f"sum is : {final_val}")
-
-
--- problem_level_1/problem_level_2/Count_digits.py
+++
@@ -1,8 +0,0 @@
-Num = int(input("Enter Your Number : "))
-count = 0 
-while Num:
-    n = Num % 10 
-    Num = Num // 10
-    count += 1
-
-print(f"Total Number of Digits are : {count}")
